1dacdc503304e5210c8c1990e331347a4de76a65
bogsti <37070968+bogsti@users.noreply.github.com>
2018-05-07 15:27:03 +0200

    Update nba-cdn.js

--- nba-cdn.js
+++ nba-cdn.js
@@ -5,6 +5,24 @@
 function nbaActionsClick(arg) {
   alert(arg);
   // document.getElementsByClassName('nba-container')[0].style.display = 'none';
+  
+//   var host = 'http://localhost:8080'; 
+//   var url = '/api/interaction/e3ef5c0e-6c1e-4c39-9b3e-b5b5b8f72cce/click';
+
+//   var XHR = new XMLHttpRequest();
+
+//   XHR.onreadystatechange = function() {
+//     if(XHR.readyState == 4) {
+//       if(XHR.status == 200) {
+//         // console.log('SUCCESS!');
+//       } else {
+//         console.log('ERROR')
+//       }
+//     }
+//   }
+
+//   XHR.open('POST', host + url);
+//   XHR.send();
 }
 
 function nbaActionsReject(arg) {
